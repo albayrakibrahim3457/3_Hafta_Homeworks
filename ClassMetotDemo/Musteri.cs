cacae542699bb259f978b82d5f63c7ea8263382e
albayrakibrahim3457 <77547591+albayrakibrahim3457@users.noreply.github.com>
2023-03-03 17:56:03 +0300

    first commit

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        int Id = "001";
+        string Adi = "";
+        string Soyadi = "";
+        string TC = "";
+
+    }
+}
